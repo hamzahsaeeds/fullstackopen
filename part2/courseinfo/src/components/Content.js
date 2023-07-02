24cb9e58c38d30d4e225216c85d27cfd584b706a
Hamzah Saeed <hamzahsaeed23@hotmail.com>
2023-07-02 16:21:00 +0500

    making it dynamic by using map function for parts

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -3,15 +3,14 @@ import Part from "./Part";
 const Content = ({ parts }) => {
   return (
     <>
-      <Part
-        part={parts[0]} 
-      />
-      <Part
-        part={parts[1]} 
-      />
-      <Part
-        part={parts[2]} 
-      />      
+      {parts.map(part => {
+        return (
+          <Part
+            key={part.id}
+            part={part}
+          />
+        )
+      })}    
     </>
   )
 }
