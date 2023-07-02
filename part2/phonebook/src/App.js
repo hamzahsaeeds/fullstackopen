3d6aa48cf3ee896f0b50674cf2e423714791eccc
Hamzah Saeed <hamzahsaeed23@hotmail.com>
2023-07-02 22:26:04 +0500

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,7 +2,10 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
@@ -21,6 +24,26 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleSearchChange = (event) => {
+    event.preventDefault();
+    const searchQuery = event.target.value.toLowerCase();
+  
+    if (searchQuery === "") {
+      setPersons([
+        { name: "Arto Hellas", number: "040-123456", id: 1 },
+        { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+        { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+        { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+      ]);
+    } else {
+      const searchedPeople = persons.filter((person) =>
+        person.name.toLowerCase().includes(searchQuery)
+      );
+      setPersons(searchedPeople);
+    }
+  };
+  
+
   const handleSubmit = (event) => {
     event.preventDefault();
     if (!isNameExists(newName)) {
@@ -36,6 +59,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with:{" "}
+        <input onChange={(event) => handleSearchChange(event)} />
+      </div>
+      <h2>add a new</h2>
       <form>
         <div>
           name:{" "}
