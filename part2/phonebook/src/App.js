643f74851f6a19dcf47c21dccdc92fa72542cf44
Hamzah Saeed <hamzahsaeed23@hotmail.com>
2023-07-02 23:39:26 +0500

    2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
@@ -32,12 +28,7 @@ const App = () => {
     const searchQuery = event.target.value.toLowerCase();
 
     if (searchQuery === "") {
-      setPersons([
-        { name: "Arto Hellas", number: "040-123456", id: 1 },
-        { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-        { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-        { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-      ]);
+      return;
     } else {
       const searchedPeople = persons.filter((person) =>
         person.name.toLowerCase().includes(searchQuery)
@@ -57,6 +48,16 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
     }
   };
+
+  const getPersonsFromJSONServer = () => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }
+
+  useEffect(() => {
+    getPersonsFromJSONServer();
+  }, []);
 
   return (
     <div>
